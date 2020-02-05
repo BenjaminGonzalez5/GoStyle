24e6646934f01a069853c08fdb9d8e63f59a7c6f
matthieu.rodrigues <matthieu.rodrigues@epsi.fr>
2020-02-05 11:16:51 +0100

    Update front connexion

--- app/src/main/java/fr/epsi/gostyle/GoStyleActivity.java
+++ app/src/main/java/fr/epsi/gostyle/GoStyleActivity.java
@@ -9,34 +9,10 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GoStyleActivity extends AppCompatActivity implements View.OnClickListener {
-    //protected DemoApp demoApp;
+public class GoStyleActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //demoApp=(DemoApp) getApplication();
-    }
-
-    protected void showBackBtn(){
-        ImageView imageView=findViewById(R.id.imageViewClose);
-        if(imageView!=null) {
-            imageView.setVisibility(View.VISIBLE);
-            imageView.setOnClickListener(this);
-        }
-    }
-
-
-    protected void displayToast(String msg){
-        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.imageViewClose:
-                finish();
-                break;
-        }
     }
 }
